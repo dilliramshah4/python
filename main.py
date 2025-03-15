c79bb9562ad3f23cba48e2618a3c24dbf07c41f5
Dilli Ram Shahu <dilliramshah4@gmail.com>
2025-03-16 01:17:57 +0530

    Add CI/CD workflow and basic arithmetic functions

---
+++ main.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print(f"Addition: {add(3, 2)}")
+    print(f"Subtraction: {subtract(3, 2)}")
